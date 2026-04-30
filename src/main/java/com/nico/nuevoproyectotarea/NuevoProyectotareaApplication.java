de95daf9966e3a0ba3d73ac8126fab67d2e13f82
nrepo58 <rodonico58@gmail.com>
2026-04-30 00:50:28 -0300

    tarea

---
+++ src/main/java/com/nico/nuevoproyectotarea/NuevoProyectotareaApplication.java
@@ -0,0 +1,13 @@
+package com.nico.nuevoproyectotarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuevoProyectotareaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuevoProyectotareaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nico/nuevoproyectotarea/NuevoProyectotareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nico.nuevoproyectotarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuevoProyectotareaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
